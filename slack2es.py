0bac876889bd05a9ff99ba127b9f9e4e4679ae3c
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2019-02-08 01:41:53 +0200

    Updated ES template to reduce the number of fields

--- slack2es.py
+++ slack2es.py
@@ -18,7 +18,7 @@ def slack2es(es_url, folder):
     print('Done!')
 
 def index(es, channel, folder, filename):
-    print('indexing {}'.format(filename))
+    print('indexing "{}"'.format(filename))
     with open(os.path.join(folder, filename)) as file:
         data = json.load(file)
 
@@ -27,10 +27,6 @@ def index(es, channel, folder, filename):
         reactions = message.get('reactions', [])
         ts = float(message['ts'])*1000
         message['ts'] = int(ts)
-
-        for msg_attachment in [{**attachment, 'channel': channel, 'type': 'attachment', 'ts': message['ts'], 'msg_text': message['text'], 'msg_user': message.get('username')} for attachment in attachments]:
-            es.index(index='slack', doc_type='slack', 
-                    body=msg_attachment)
         
         for reaction in [{**reaction, 'type': 'reaction', 'channel': channel, 'ts': message['ts'], 'msg_text': message['text'], 'msg_user': message.get('username')} for reaction in reactions]:
             es.index(index='slack', doc_type='slack', 
