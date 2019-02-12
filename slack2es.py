e35701c9409916e75d814e7b0d05e89e285dabcf
Avishai Ish-Shalom <avishai.ishshalom@gmail.com>
2019-02-12 16:57:14 +0200

    refactor indexing to use bulk

--- slack2es.py
+++ slack2es.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 import click
 from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
 from datetime import datetime
 import json
 import os.path, os
 import copy
+import itertools
 
 @click.command()
 @click.option('--es-url', default='http://localhost:9200/')
@@ -17,25 +19,37 @@ def slack2es(es_url, folder):
         index(es, channel, folder, file)
     print('Done!')
 
+
+def fix_ts(message):
+    ts = float(message['ts'])*1000
+    return {**message, 'ts': int(ts)}
+
+
+def to_reaction_doc(channel, reaction, message):
+    return {**reaction, 'type': 'reaction', 'channel': channel, 'ts': message['ts'], 'msg_text': message['text'], 'msg_user': message.get('username')}
+
+
+def reaction_id(channel, reaction):
+    return '-'.join(['reaction', channel, str(reaction['ts']), reaction['name']])
+
+
+def to_message_doc(channel, message):
+    return {**message, 'channel': channel}
+
+
+def message_id(channel, message):
+    return '-'.join([channel, str(message['ts'])])
+
+
 def index(es, channel, folder, filename):
     print('indexing "{}"'.format(filename))
     with open(os.path.join(folder, filename)) as file:
         data = json.load(file)
 
-    for message in data:
-        attachments = message.get('attachments', [])
-        reactions = message.get('reactions', [])
-        ts = float(message['ts'])*1000
-        message['ts'] = int(ts)
-        
-        for reaction in [{**reaction, 'id': '-'.join(['reaction', channel, message['ts'], reaction['name']]),  'type': 'reaction', 'channel': channel, 'ts': message['ts'], 'msg_text': message['text'], 'msg_user': message.get('username')} for reaction in reactions]:
-            es.index(index='slack', doc_type='slack', 
-                    body=reaction)      
-
-        es.index(index='slack', doc_type='slack', 
-            body={**message, 'type': 'message', 'channel': channel, 'id': '-'.join(['message', channel, message['ts']])}
-            )
-
+    messages = [fix_ts(msg) for msg in data]
+    reactions = itertools.chain.from_iterable([to_reaction_doc(channel, reaction, message) for reaction in message.get('reactions', [])] for message in messages)
+    bulk(es, ({'_index': 'slack', '_type': 'slack', '_source': reaction, '_id': reaction_id(channel, reaction)} for reaction in reactions))
+    bulk(es, ({'_index': 'slack', '_type': 'slack', '_source': to_message_doc(channel, message), '_id': message_id(channel, message)} for message in messages))
     print('Done indexing {}'.format(filename))
 
 
